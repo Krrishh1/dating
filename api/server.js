1c81dddfcc63818b6156d4270e2e8ed96bff2836
krishna Rajput <krishnarajput4045@gmail.com>
2025-08-02 20:12:49 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -23,8 +23,9 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 initializeSocket(httpServer);
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
-app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
